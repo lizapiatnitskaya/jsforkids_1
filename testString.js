f52c415557123066609fbad2afb46fd9da0f74f0
lizapiatnitskaya <piatnitskayaliza@gmail.com>
2023-05-09 13:05:42 +0300

    testString

---
+++ testString.js
@@ -0,0 +1,7 @@
+var myString="Какая прекрасная погода на улице!";
+var sadString="(нет)";
+var realString=myString+sadString;
+console.log(realString);
+console.log(realString.length);
+console.log(realString.slice(4));
+console.log(realString[3]);
