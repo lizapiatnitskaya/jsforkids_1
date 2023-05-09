f3d82064c3c6a061cd343d79af23ed64b78bf808
lizapiatnitskaya <piatnitskayaliza@gmail.com>
2023-05-09 13:19:40 +0300

    boolean

---
+++ booloperations.js
@@ -0,0 +1,10 @@
+var myTrue = true;
+var myFalse= false;
+console.log(myTrue&&myFalse);
+console.log(myTrue||myFalse);
+console.log(!myTrue);
+
+var age=13;
+var permission = age>=12;
+var accompanied=false;
+console.log(permission||accompanied);
