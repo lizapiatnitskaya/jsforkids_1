8602c498191ce4a33b02cd9f029605a8d8ea408c
lizapiatnitskaya <piatnitskayaliza@gmail.com>
2023-05-10 14:14:48 +0300

    add bank and myCrazyObject

---
+++ exercises_from _book/bank.js
@@ -0,0 +1,7 @@
+let owedMoney={};
+owedMoney.bob=5;
+owedMoney.margaret=3;
+owedMoney.bob+=3;
+console.log(owedMoney);
+
+   
---
+++ exercises_from _book/meCrazyObject.js
@@ -0,0 +1,6 @@
+var myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3],
+    "random animal": "Банановая акула"
+    };
+console.log(myCrazyObject["some array"][2].number); 
